611218f302d30213fece13c1a8997f87a44afa70
Jacob Senecal <jacob.senecal@gmail.com>
2017-04-27 13:55:42 -0500

    Add databases to dashboard context

--- djangosqladmin/databases/views.py
+++ djangosqladmin/databases/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def dashboard(request):
-    return HttpResponse('SUCCESS!')
+    context = {}
+    if request.user.is_authenticated:
+        context['databases'] = request.user.database_set.all()
+    return render(request, 'databases/dashboard.html', context)
